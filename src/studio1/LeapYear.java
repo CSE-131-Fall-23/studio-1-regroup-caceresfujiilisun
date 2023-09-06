044b719e3858da411107f236999a376089952b42
Isabella <Isabella@DESKTOP-TSISTBU.dhcp.wustl.edu>
2023-09-06 16:55:27 -0500

    We did it!

---
+++ src/studio1/LeapYear.java
@@ -0,0 +1,14 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class LeapYear {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Input a year.");
+		int year = in.nextInt(); 
+		
+		boolean isleapyear = ((year % 4 == 0 ) && (year % 100 != 0)) || (year % 400 == 0);
+		System.out.println(isleapyear); 
+	}
+}
